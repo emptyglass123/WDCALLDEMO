db770e052520e6e1d673c0771bbec336ea99670e
Apple <apple@AppledeMac-mini.local>
2017-05-03 17:41:17 +0800

    上传

---
+++ 来电识别/CallRecognize_DEMO/CallRecognize_DEMO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CallRecognize_DEMO
+//
+//  Created by 朱辉 on 2017/5/3.
+//  Copyright © 2017年 jxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 来电识别/CallRecognize_DEMO/CallRecognize_DEMO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CallRecognize_DEMO
+//
+//  Created by 朱辉 on 2017/5/3.
+//  Copyright © 2017年 jxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
